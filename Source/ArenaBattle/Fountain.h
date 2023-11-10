491556ab0927d14d9159fb54d6ac8a6254daf4e0
nizi123 <hee981226@naver.com>
2023-11-10 13:44:38 +0900

    분수대 Acotr 생성, 생성자 주입

--- Source/ArenaBattle/Fountain.h
+++ Source/ArenaBattle/Fountain.h
@@ -2,7 +2,8 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+//#include "CoreMinimal.h"
+#include "EngineMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
@@ -23,4 +24,18 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Body;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent *Light;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent *Splash;
+
+	UPROPERTY(EditAnywhere, Category=ID)
+	int32 ID;
 };
