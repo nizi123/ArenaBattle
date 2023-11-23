a252b7a46c6054e7cb8dc4032b683bc872b1b937
nizi123 <hee981226@naver.com>
2023-11-23 13:10:22 +0900

    UMG 모듈 추가

--- Source/ArenaBattle/ArenaBattle.Build.cs
+++ Source/ArenaBattle/ArenaBattle.Build.cs
@@ -8,6 +8,9 @@ public class ArenaBattle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
-	}
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "UMG" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+    }
 }
